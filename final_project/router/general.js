bff99bcc3d9dae9c251f5ad867df992f14863ac4
Eleazar-ZOUBGA <eleazarzoubga@gmail.com>
2025-06-08 09:55:20 -0400

    end task 4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,26 +12,29 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(200).json(books);
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const book = books[req.params.isbn];
+    if (book) {
+        return res.status(200).json(book);
+    } else {
+        return res.status(404).json({ message: "Book not found" });
+    }
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const result = Object.values(books).filter(book => book.author === req.params.author);
+    return res.status(200).json(result);
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const result = Object.values(books).filter(book => book.title === req.params.title);
+    return res.status(200).json(result);
 });
 
 //  Get book review
